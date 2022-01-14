69ecf7ab4190fe3790e5c09f646817e94add85d9
vuser <vivCoding@gmail.com>
2022-01-14 00:25:35 -0500

    topic0 B

--- 290cp1/topic0/B.cpp
+++ 290cp1/topic0/B.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 /*
  * Problem: https://purdue.kattis.com/sessions/zfo7ia/problems/joinstrings
- * UNSOLVED, runtime error after first few test cases
 */
 
-void print(int start, vector<vector<int>> m, string* s) {
-	auto ps = m.at(start);
+void print(int start, vector<int>* m, string* s, int n) {
+	auto ps = m[start];
 	cout << s[start];
 	for (auto p = ps.begin(); p != ps.end(); p++) {
-		print(*p, m, s);
+		print(*p, m, s, n);
 	}
 }
 
@@ -19,19 +18,18 @@ signed main() {
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	int n; cin >> n;
 	string s[n];
-	vector<vector<int>> m;
+	vector<int> m[n];
 	for (int i = 0; i < n; i++) {
 		cin >> s[i];
-		vector<int> m2;
-		m.push_back(m2);
+		m[i] = vector<int>();
 	}
-	int last = -1;
+	int last = 0;
 	for (int i = 0; i < n - 1; i++) {
 		int a, b; cin >> a >> b;
-		auto pb = m.at(b - 1);
-		m.at(a - 1).push_back(b - 1);
-		last = a - 1;
+		a--; b--;
+		m[a].push_back(b);
+		last = a;
 	}
-	print(last, m, s);
+	print(last, m, s, n);
     return 0;
 }
