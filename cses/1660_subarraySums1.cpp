884137dc4291d7b830bb811771cee6621ffb0ea8
vivCoding <vivCoding@gmail.com>
2022-04-06 00:42:45 -0400

    subarray

--- cses/1660_subarraySums1.cpp
+++ cses/1660_subarraySums1.cpp
@@ -9,10 +9,36 @@ const ll MOD9 = 1e9 + 7;
 
 /*
  * Problem: https://cses.fi/problemset/task/1660
- TODO
 */
 
 signed main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ll n, x; cin >> n >> x;
+    ll nums[n];
+    for (ll i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    ll i = 0, j = 0;
+    ll ct = 0;
+    ll sum = nums[i];
+    while (true) {
+        if (sum == x) ct++;
+        if (i == j) {
+            if (j == n - 1) break;
+            else {
+                sum += nums[++j];
+                continue;
+            }
+        }
+        if (sum <= x) {
+            if (j == n - 1) break;
+            else {
+                sum += nums[++j];
+            }
+        } else {
+            sum -= nums[i++];
+        }
+    }
+    cout << ct << endl;
     return 0;
 }
