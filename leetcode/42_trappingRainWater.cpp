f1cc68611c58f8577d31bc1a5a86a678dcd2c358
vuser <vivCoding@gmail.com>
2022-11-01 01:15:35 -0400

    jazz

--- leetcode/42_trappingRainWater.cpp
+++ leetcode/42_trappingRainWater.cpp
@@ -5,7 +5,7 @@ using namespace std;
  * Problem: https://leetcode.com/problems/trapping-rain-water/
 */
 
-int trap(vector<int>& height) {
+int trap2(vector<int>& height) {
     int n = height.size();
     stack<pair<int, int>> s1, s2;
     for (int i = n - 1; i >= 0; i--) {
@@ -38,9 +38,31 @@ int trap(vector<int>& height) {
     return ct;
 }
 
+int trap(vector<int>& height) {
+    int n = height.size();
+    int lmx[n], rmx[n];
+    int mx = 0;
+    for (int i = 0; i < n; i++) {
+        mx = max(mx, height[i]);
+        lmx[i] = mx;
+    }
+
+    mx = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        mx = max(mx, height[i]);
+        rmx[i] = mx;
+    }
+
+    int ct = 0;
+    for (int i = 0; i < n; i++) {
+        ct += min(lmx[i], rmx[i]) - height[i];
+    }
+    return ct;
+}
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cout << LONG_MAX << endl;
-    cout << LONG_LONG_MAX << endl;
+    vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
+    cout << trap(height) << endl;
     return 0;
 }
